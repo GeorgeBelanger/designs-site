d2b60cb4bd9cf9570746f0c4cff77845788db9eb
thrugsis <realtorgeorgeb@gmail.com>
2020-01-06 22:20:17 -0500

    added background

--- js/designs.js
+++ js/designs.js
@@ -717,6 +717,17 @@ console.log('Thanks for checking out the code for my website! You can get a full
   
   //Reload on Resize to recalculate every thing
   //window.onresize = function(){ location.reload(); }
+  var SEPARATION = 100, AMOUNTX = 50, AMOUNTY = 50;
+
+  var container;
+  var camera, scene, renderer;
+
+  var particles, count = 0;
+
+  var mouseX = 0, mouseY = 0;
+
+  var windowHalfX = window.innerWidth / 2;
+  var windowHalfY = window.innerHeight / 2;
 
 
 function init() {
@@ -731,7 +742,9 @@ function init() {
   root.camera.position.set(0, 0, 600);
   
   var textAnimation = createTextAnimation();
+  var particleAnimation = createParticleAnimation();
   root.scene.add(textAnimation);
+  root.scene.add(particleAnimation);
   
   var light = new THREE.DirectionalLight();
   light.position.set(0, 0, 1);
@@ -787,6 +800,54 @@ function generateTextGeometry(text, params) {
   geometry.applyMatrix(matrix);
 
   return geometry;
+}
+
+function createParticleAnimation(){
+  var numParticles = AMOUNTX * AMOUNTY;
+
+  var positions = new Float32Array( numParticles * 3 );
+  var scales = new Float32Array( numParticles );
+
+  var i = 0, j = 0;
+
+  for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+
+    for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+
+      positions[ i ] = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 ); // x
+      positions[ i + 1 ] = 0; // y
+      positions[ i + 2 ] = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 ); // z
+
+      scales[ j ] = 1;
+
+      i += 3;
+      j ++;
+
+    }
+
+  }
+
+  var geometry = new THREE.BufferGeometry();
+  geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+  geometry.addAttribute( 'scale', new THREE.BufferAttribute( scales, 1 ) );
+
+  var material = new THREE.ShaderMaterial( {
+
+    uniforms: {
+      color: {type: 'c', value: new THREE.Color( 0xffffff ) },
+    },
+    
+    vertexShader: document.getElementById( 'vertexshader' ).textContent,
+    fragmentShader: document.getElementById( 'fragmentshader' ).textContent
+
+  } );
+
+  //
+  console.log(1)
+  particles = new THREE.Points( geometry, material );
+  
+    // scene.add( particles );
+  return particles
 }
 
 ////////////////////
@@ -964,13 +1025,44 @@ THREERoot.prototype = {
     this.controls && this.controls.update();
   },
   render: function() {
-    this.renderer.render(this.scene, this.camera);
-    window.scene = this.scene;
+    console.log(2)
+    setTimeout(() =>{
+      console.log(3)
+      
+      var positions = particles.geometry.attributes.position.array;
+      var scales = particles.geometry.attributes.scale.array;
+      
+      var i = 0, j = 0;
+      
+      for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+        
+        for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+          
+          positions[ i + 1 ] = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) +
+          ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
+
+          scales[ j ] = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 8 +
+          ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 8;
+          
+          i += 3;
+          j ++;
+          
+        }
+        
+      }
+      
+      particles.geometry.attributes.position.needsUpdate = true;
+      particles.geometry.attributes.scale.needsUpdate = true;
+      this.renderer.render(this.scene, this.camera);
+      window.scene = this.scene;
+      count += 0.1;
+
+    },100)
   },
   resize: function() {
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
-
+    
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 };
